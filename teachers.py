732a5685ba8d2960df045e505ddaf744a2f547fa
syntarch <alexeysyutkin@gmail.com>
2020-02-05 22:40:31 +0300

    Teacher's pages - timetable

--- teachers.py
+++ teachers.py
@@ -29,12 +29,21 @@ def teachers(teacher_id):
             about = prepod['about']
             picture = prepod['picture']
             for_goals = prepod['goals']
+            timetable = prepod['free']
     teacher_goals = []
+    time_available = {}
+    for day, times in timetable.items():
+        time_available[day] = {}
+        for time, status in times.items():
+            if status is True:
+                time_available[day][time] = True
     for x in goals_list:
         if x in for_goals:
             teacher_goals += [goals_list[x]]
+    week = {'mon': 'Понедельник', 'tue': 'Вторник', 'wed': 'Среда', 'thu': 'Четверг', 'fri': 'Пятница', 'sat': 'Суббота',
+            'sun': 'Воскресенье'}
     return render_template('profile.html', name=name, rating=rating, price=price, about=about, picture=picture,
-                           teacher_goals=teacher_goals)
+                           teacher_goals=teacher_goals, schedule=time_available, week=week)
 
 @app.route('/request/')
 def request():
